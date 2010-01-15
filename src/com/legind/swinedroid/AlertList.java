b137f481c084dcbee39248350f24af329cb6ce75
William Budington <bill.budington@gmail.com>
2010-01-15 01:37:12 -0500

    Begin AlertList layout, added alert graphics

--- src/com/legind/swinedroid/AlertList.java
+++ src/com/legind/swinedroid/AlertList.java
@@ -1,7 +1,37 @@
 package com.legind.swinedroid;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 import android.app.ListActivity;
+import android.os.Bundle;
+import android.widget.SimpleAdapter;
 
 public class AlertList extends ListActivity{
+	ArrayList<HashMap<String,String>> list = new ArrayList<HashMap<String,String>>();
+	
+    @Override
+	public void onCreate(Bundle savedInstanceState) {
+    	super.onCreate(savedInstanceState);
+    	setContentView(R.layout.server_list);
+    	fillData();
+    	registerForContextMenu(getListView());
+	}
 
+    private void fillData() {
+		HashMap<String,String> item = new HashMap<String,String>();
+		item.put("sig_name","blah blah alert threat [emegingthreats]");
+		item.put("ip_src","Source IP: 192.168.1.1");
+		item.put("ip_dst","Destination IP: 192.168.1.2");
+		item.put("timestamp_date","2012-10-10");
+		item.put("timestamp_time","10:11:10");
+		list.add(item);
+		item.put("sig_name","blah blah alert threat [emegingthreats]");
+		item.put("ip_src","Source IP: 192.168.1.1");
+		item.put("ip_dst","Destination IP: 192.168.1.2");
+		item.put("timestamp_date","2012-10-10");
+		item.put("timestamp_time","10:11:10");
+		list.add(item);
+		setListAdapter(new SimpleAdapter(this, list, R.layout.alert_row, new String[] {"sig_name", "ip_src", "ip_dst", "timestamp_date", "timestamp_time"}, new int[] {R.id.alert_row_sig_name_text, R.id.alert_row_ip_src_text, R.id.alert_row_ip_dst_text, R.id.alert_row_date_text, R.id.alert_row_time_text}));
+    }
 }
--- src/com/legind/swinedroid/AlertSearch.java
+++ src/com/legind/swinedroid/AlertSearch.java
@@ -8,7 +8,6 @@ import android.app.Dialog;
 import android.app.TimePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.view.Window;
 import android.widget.ArrayAdapter;
